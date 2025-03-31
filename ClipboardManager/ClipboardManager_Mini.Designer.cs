10a5a4cb7165aabece5be7c36e3c04f714e671c9
CodeCoddington <vinchenzo.jackson@gmail.com>
2025-03-31 11:18:28 -0400

    Started mini code
    Started code on mini form.

--- ClipboardManager/ClipboardManager_Mini.Designer.cs
+++ ClipboardManager/ClipboardManager_Mini.Designer.cs
@@ -30,23 +30,23 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClipboardManager_Mini));
             this.gb_cmSmall = new System.Windows.Forms.GroupBox();
+            this.pb_smallToMedium_buttonFace = new System.Windows.Forms.PictureBox();
             this.btn_smallToMedium = new System.Windows.Forms.Button();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pb_detectSignal = new System.Windows.Forms.PictureBox();
+            this.pb_box = new System.Windows.Forms.PictureBox();
             this.gb_cmSmall.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_smallToMedium_buttonFace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_detectSignal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_box)).BeginInit();
             this.SuspendLayout();
             // 
             // gb_cmSmall
             // 
             this.gb_cmSmall.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.gb_cmSmall.Controls.Add(this.pictureBox3);
+            this.gb_cmSmall.Controls.Add(this.pb_smallToMedium_buttonFace);
             this.gb_cmSmall.Controls.Add(this.btn_smallToMedium);
-            this.gb_cmSmall.Controls.Add(this.pictureBox2);
-            this.gb_cmSmall.Controls.Add(this.pictureBox1);
+            this.gb_cmSmall.Controls.Add(this.pb_detectSignal);
+            this.gb_cmSmall.Controls.Add(this.pb_box);
             this.gb_cmSmall.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold);
             this.gb_cmSmall.ForeColor = System.Drawing.Color.White;
             this.gb_cmSmall.Location = new System.Drawing.Point(0, 0);
@@ -55,6 +55,17 @@
             this.gb_cmSmall.TabIndex = 0;
             this.gb_cmSmall.TabStop = false;
             this.gb_cmSmall.Text = "Mini";
+            // 
+            // pb_smallToMedium_buttonFace
+            // 
+            this.pb_smallToMedium_buttonFace.BackColor = System.Drawing.SystemColors.Control;
+            this.pb_smallToMedium_buttonFace.BackgroundImage = global::ClipboardManager.Properties.Resources.MiniToMedium_Arrows;
+            this.pb_smallToMedium_buttonFace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb_smallToMedium_buttonFace.Location = new System.Drawing.Point(16, 85);
+            this.pb_smallToMedium_buttonFace.Name = "pb_smallToMedium_buttonFace";
+            this.pb_smallToMedium_buttonFace.Size = new System.Drawing.Size(28, 37);
+            this.pb_smallToMedium_buttonFace.TabIndex = 2;
+            this.pb_smallToMedium_buttonFace.TabStop = false;
             // 
             // btn_smallToMedium
             // 
@@ -67,54 +78,42 @@
             this.btn_smallToMedium.TabIndex = 1;
             this.btn_smallToMedium.UseVisualStyleBackColor = false;
             // 
-            // pictureBox2
+            // pb_detectSignal
             // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox2.Location = new System.Drawing.Point(10, 29);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(41, 41);
-            this.pictureBox2.TabIndex = 0;
-            this.pictureBox2.TabStop = false;
+            this.pb_detectSignal.BackColor = System.Drawing.SystemColors.Control;
+            this.pb_detectSignal.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pb_detectSignal.BackgroundImage")));
+            this.pb_detectSignal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb_detectSignal.Location = new System.Drawing.Point(10, 29);
+            this.pb_detectSignal.Name = "pb_detectSignal";
+            this.pb_detectSignal.Size = new System.Drawing.Size(41, 41);
+            this.pb_detectSignal.TabIndex = 0;
+            this.pb_detectSignal.TabStop = false;
             // 
-            // pictureBox1
+            // pb_box
             // 
-            this.pictureBox1.BackgroundImage = global::ClipboardManager.Properties.Resources.ControlBox_Only2;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox1.Location = new System.Drawing.Point(4, 23);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(53, 53);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pb_box.BackgroundImage = global::ClipboardManager.Properties.Resources.ControlBox_Only2;
+            this.pb_box.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb_box.Location = new System.Drawing.Point(4, 23);
+            this.pb_box.Name = "pb_box";
+            this.pb_box.Size = new System.Drawing.Size(53, 53);
+            this.pb_box.TabIndex = 0;
+            this.pb_box.TabStop = false;
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox3.BackgroundImage = global::ClipboardManager.Properties.Resources.MiniToMedium_Arrows;
-            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox3.Location = new System.Drawing.Point(16, 85);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(28, 37);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            // 
-            // ClipboardManager_Small
+            // ClipboardManager_Mini
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(61, 135);
+            this.ClientSize = new System.Drawing.Size(61, 134);
             this.Controls.Add(this.gb_cmSmall);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "ClipboardManager_Small";
-            this.Text = "CM";
+            this.Name = "ClipboardManager_Mini";
             this.gb_cmSmall.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_smallToMedium_buttonFace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_detectSignal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_box)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -122,10 +121,10 @@
         #endregion
 
         private System.Windows.Forms.GroupBox gb_cmSmall;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pb_detectSignal;
+        private System.Windows.Forms.PictureBox pb_box;
         private System.Windows.Forms.Button btn_smallToMedium;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pb_smallToMedium_buttonFace;
     }
 }
 
--- ClipboardManager/ClipboardManager_Mini.cs
+++ ClipboardManager/ClipboardManager_Mini.cs
@@ -12,9 +12,6 @@ namespace ClipboardManager
 {
     public partial class ClipboardManager_Mini: Form
     {
-        //---CLASSS VARS---
-
-
         //---CONSTRUCTOR---
         public ClipboardManager_Mini()
         {
@@ -24,13 +21,40 @@ namespace ClipboardManager
 
         private void InitializeEvents()
         {
+            // Form
+            this.Load += ClipboardManager_Mini_Load;
             this.Shown += ClipboardManager_Small_Shown;
+
+            // Buttons and related
+            btn_smallToMedium.Click += Btn_smallToMedium_Click;
+            pb_smallToMedium_buttonFace.Click += Pb_smallToMedium_buttonFace_Click;
         }
 
         //---FORM LOAD / SHOWN---
+        private void ClipboardManager_Mini_Load(object sender, EventArgs e)
+        {
+            InitializeControlProperties();
+        }
+
+        private void InitializeControlProperties()
+        {
+            pb_detectSignal.Visible = false;
+        }
+
         private async void ClipboardManager_Small_Shown(object sender, EventArgs e)
         {
             await Task.Delay(500);
+        }
+
+        private void Pb_smallToMedium_buttonFace_Click(object sender, EventArgs e)
+        {
+            btn_smallToMedium.PerformClick();
+        }
+
+        private void Btn_smallToMedium_Click(object sender, EventArgs e)
+        {
+            // Will launch a larger form with more features.
+            throw new NotImplementedException();
         }
     }
 }
